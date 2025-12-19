c9bdfb86ca0e2b0200d44631b24f10e7fa3951b5
Vardan2009 <Vardan2009@users.noreply.github.com>
2025-12-19 14:31:11 +0000

    deploy: be82cef2c1544c7b9859113ef1967486c4077f20

--- app/assets/api-Bfh7xik8.js
+++ app/assets/api-Bfh7xik8.js
@@ -1,3 +1,3 @@
-import{y as e}from"./index-DMYWl0Kz.js";const s=`https://time-manager-app-backend.onrender.com
+import{z as e}from"./index-C4wfkl3G.js";const s=`https://time-manager-app-backend.onrender.com
 
 `;async function i(n,r={}){const a={"Content-Type":"application/json",...r.headers||{}};e.state.accessToken&&(a.Authorization=`Bearer ${e.state.accessToken}`);const o=await fetch(`${s}${n}`,{...r,headers:a}),t=await o.json();if(t.error)throw new Error(t.error);if(o.status===401)throw e.logout(),new Error("Unauthorized");return t}export{i as a};
